ff100b22d63a966c6a9927fbc77a6a9dcb67d096
Sparklegift <129224621+Sparklegift@users.noreply.github.com>
2023-04-22 01:41:21 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
- /**
-  * main: void
-  * Return: integer 0
-  */
- int main(void)
- {
-	 puts("\"Programming is like building a multilingual puzzle");
-	 return (0);
- }
+/**
+* main: void
+* Return: integer 0
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
