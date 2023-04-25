f41b6a082952ac5942beff4f999b3d684de835ac
Sparklegift <godsgiftetuk2001@gmail.com>
2023-04-25 16:40:54 -0700

    Advance task

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - void
+ * Return: 0
+ */
+int main(void)
+{
+	int num1, num2;
+	/*ASCII code for 0*/
+
+	for (num1 = 48; num1 < 58; num1++)
+	
+	{
+
+		for (num2 = 49; num2 < 58; num2++)
+
+		{
+			if (num1 != num2 && num2 > num1)
+			{
+				putchar(num1);
+				putchar(num2);
+				if (num1 < 56)
+				{
+				putchar(',');
+				putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
