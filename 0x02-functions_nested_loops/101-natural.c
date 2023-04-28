2a5e456433f8d71e76e1835597cc03f14861289d
Sparklegift <godsgiftetuk2001@gmail.com>
2023-04-28 16:24:14 -0700

    All else is the work of women

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -5,18 +5,18 @@
  */
 int main(void)
 {
-  int n = 1;
-  int sum = 0;
+	int n = 1;
+	int sum = 0;
 
-  while (n < 1024)
-  {
+	while (n < 1024)
+	{
 
-    if (n%3 == 0 || n%5 == 0)
-    {
-      sum = sum + n;
-    }
-    n++;
-  }
-  printf("%d\n", sum);
-  return (0);
+		if (n%3 == 0 || n%5 == 0)
+		{
+			sum = sum + n;
+		}
+		n++;
+	}
+	printf("%d\n", sum);
+	return (0);
 }
