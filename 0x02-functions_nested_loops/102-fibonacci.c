2bb41b5a88ce7199f5072766a70448791d514663
Sparklegift <godsgiftetuk2001@gmail.com>
2023-04-28 16:42:47 -0700

    Advance Taskes

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - void
+ * Return: 0
+ */
+int main(void)
+{
+	int a = 1, b = 2, c, i;
+	printf("%d, %d, ", a, b);
+
+	for (i = 2; i < 50; i++)
+	{
+		c = a + b;
+		printf("%d, ", c);
+		a = b;
+		b = c;
+	}
+	printf("\n");
+	return (0);
+}
