37d3f09bcfbf591f394fd05464784da756755c17
Sparklegift <129224621+Sparklegift@users.noreply.github.com>
2023-04-29 00:08:32 +0100

    Update 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
+/**
+ * main - void
+ * Return: 0
+ */
 int main(void)
 {
   int n = 1;
   int sum = 0;
+
   while (n < 1024)
   {
+
     if (n%3 == 0 || n%5 == 0)
     {
       sum = sum + n;
     }
     n++;
   }
-    printf("%d\n", sum);
-    return (0);
+  printf("%d\n", sum);
+  return (0);
 }
