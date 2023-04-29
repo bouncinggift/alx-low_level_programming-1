797f8e6b37e61fd5ff93db39f2a4a29c59ee266a
Sparklegift <129224621+Sparklegift@users.noreply.github.com>
2023-04-29 01:58:53 +0100

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -20,7 +20,7 @@ void print_times_table(int n)
 			_putchar(' ');
 
 			if (prod <= 9)
-			printf(' ');
+			_putchar(' ');
 			else
 				_putchar((prod / 10) + '0');
 			_putchar((prod % 10) + '0');
