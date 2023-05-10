31d9a897931672629ad7653dfa99cc8a0dd34bb1
Godsgift Robinson <94372591+bouncinggift@users.noreply.github.com>
2023-05-10 05:50:05 +0100

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
  * _memset - fills memory with a constant byte.
- * @s : source string
- * @b : the constant byte for filling
- * @n : lenght of buffer
- * Return : new string.
+ * @s: source string
+ * @b: the constant byte for filling
+ * @n: lenght of buffer
+ * Return: new string.
  */
 char *_memset(char *s, char b, unsigned int n)
 {
